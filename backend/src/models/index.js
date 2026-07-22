2e8a709c3041217c619af35d93cae51ea2da950d
Sanin <sanin9841393780@gmail.com>
2026-07-22 17:16:46 +0545

    index.js

---
+++ backend/src/models/index.js
@@ -0,0 +1,11 @@
+module.exports = {
+  User: require('./User'),
+  Address: require('./Address'),
+  Category: require('./Category'),
+  Product: require('./Product'),
+  Cart: require('./Cart'),
+  Order: require('./Order'),
+  Payment: require('./Payment'),
+  Review: require('./Review'),
+  Wishlist: require('./Wishlist'),
+};
